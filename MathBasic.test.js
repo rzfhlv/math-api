02e464fbdf91e6f14c55280a3c1c0070761a90e5
rzfhlv <rezafahlevi.ilkom49@gmail.com>
2022-02-28 19:08:53 +0700

    add add test and functional

--- MathBasic.test.js
+++ MathBasic.test.js
@@ -11,4 +11,25 @@ describe('A MathBasic', () => {
     expect(MathBasic.multiply).toBeInstanceOf(Function);
     expect(MathBasic.divide).toBeInstanceOf(Function);
   });
+
+  describe('A add function', () => {
+    it('should throw error when not given 2 parameters', () => {
+      expect(() => MathBasic.add()).toThrowError();
+      expect(() => MathBasic.add(1)).toThrowError();
+      expect(() => MathBasic.add(1,2,3)).toThrowError();
+      expect(() => MathBasic.add(1,2,3,4)).toThrowError();
+    });
+
+    it('should throw error when given non-number parameters', () => {
+      expect(() => MathBasic.add('1', '2')).toThrowError();
+      expect(() => MathBasic.add(true, {})).toThrowError();
+      expect(() => MathBasic.add(null, false)).toThrowError();
+    });
+
+    it('should return a + b when given two number parameters', () => {
+      expect(MathBasic.add(2, 2)).toEqual(4);
+      expect(MathBasic.add(16, 8)).toEqual(24);
+      expect(MathBasic.add(3, 7)).toEqual(10);
+    });
+  });
 });
